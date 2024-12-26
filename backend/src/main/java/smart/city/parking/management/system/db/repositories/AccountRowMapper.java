c4c1d9d2371e810104c369da6c357e97e7dd6708
Mohamed Rezq <mohamedrezq788@gmail.com>
2024-12-26 04:23:14 +0200

    add accountRowMapper

---
+++ backend/src/main/java/smart/city/parking/management/system/db/repositories/AccountRowMapper.java
@@ -0,0 +1,20 @@
+package smart.city.parking.management.system.db.repositories;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import smart.city.parking.management.system.db.models.Account;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountRowMapper implements RowMapper<Account> {
+    @Override
+    public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Account(
+            rs.getInt("account_id"), 
+            rs.getString("username"), 
+            rs.getString("full_name"), 
+            rs.getString("role")
+        );
+    }
+}
